5198fd82b597fa26fdfd645ef8a40ca802199c9d
desivar <your.email@example.com>
2025-03-10 04:29:32 -0600

    correct mistakes

--- server.js
+++ server.js
@@ -2,52 +2,29 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
-/******************************************/
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const dotenv = require("dotenv");
-const app = express();
-const staticRoute = require("./routes/static");
+const express = require("express")
+const env = require("dotenv").config()
+const app = express()
+const static = require("./routes/static")
 
-/*************************
- * Dotenv Configuration
+/* ***********************
+ * Routes
  *************************/
-dotenv.config();
+app.use(static)
 
-/*************************
+/* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 5500; // if .env port is not set, 5500 is used.
-const host = process.env.HOST || "localhost";
+const port = process.env.PORT
+const host = process.env.HOST
 
-/**********************
- * View Engine and Layouts
- *************************/
-app.set("view engine", "ejs");
-app.use(expressLayouts);
-app.set("layout", "./layouts/layout");
-
-/**********************
- * Routes
- *************************/
-app.use(staticRoute);
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
-
-/**********************
+/* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`);
-});
+  console.log(`app listening on ${host}:${port}`)
+})
